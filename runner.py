799012ceee3d78e5ab478005666baa15a13d537a
onikarubi <printuserselect@gmail.com>
2024-08-18 06:05:08 +0900

    refactor runner

--- runner.py
+++ runner.py
@@ -2,6 +2,7 @@ from llm import CausalLM
 from tokenizer import Tokenizer
 import torch
 
+
 class ModelRunner:
     llm: CausalLM
     tokenizer: Tokenizer
@@ -16,15 +17,23 @@ class ModelRunner:
 
     def run(self, question: str):
         prompt = self.tokenizer.tokenizer.apply_chat_template(
-            conversation=[{'role': 'user', 'content': question}],
+            conversation=[{"role": "user", "content": question}],
             tokenize=False,
-            add_generation_prompt=True
+            add_generation_prompt=True,
         )
 
-        model_inputs = self.tokenizer.tokenizer([prompt], return_tensors="pt").to("cuda")
+        model_inputs = self.tokenizer.tokenizer([prompt], return_tensors="pt").to(
+            "cuda"
+        )
+        generated_ids = self.llm.model.generate(
+            model_inputs.input_ids,
+            attention_mask=model_inputs.attention_mask,
+            max_new_tokens=300,
+        )
         generated_ids = [
-        output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
-    ]
+            output_ids[len(input_ids) :]
+            for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+        ]
 
         print(generated_ids)
         return generated_ids
